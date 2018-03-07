0553b4e65636037d71114f110a815ab28f6ec9c8
Neek Sandhu <neek.sandhu@outlook.com>
2018-03-07 02:07:17 -0800

    Deprecate Endpoint.path and Endpoint#isReallyInternal

--- src/Endpoint.ts
+++ src/Endpoint.ts
@@ -1,31 +1,33 @@
 class Endpoint {
-    private _path: string;
+    private _name: string;
     constructor(path: string) {
-        this._path = path;
+        this._name = path;
     }
 
     public get path() {
-        return this._path;
+        console.warn(`'sender.path' is now deprecated, since it no longer supports iframes. Use 'sender.name' instead`)
+        return this._name;
     }
-
+    
     public isInternal(): boolean {
         return this.isInternalImpl(false);
     }
-
+    
     public isReallyInternal(): boolean {
+        console.warn(`'sender.isReallyInternal()' is now deprecated, since it no longer supports iframes. Use 'sender.isInternal()' instead`)
         return this.isInternalImpl(true);
     }
 
     public toString() {
-        return this._path;
+        return this._name;
     }
 
     private isInternalImpl(strict: boolean): boolean {
-        if (!strict && this._path.indexOf('devtools') === 0) {
+        if (!strict && this._name.indexOf('devtools') === 0) {
             return true;
         }
         // 'content-script:frame#2' || 'window@712' => false
-        return !(/window|frame/.test(this._path));
+        return !(/window|frame/.test(this._name));
     }
 }
 
