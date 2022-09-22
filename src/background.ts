56539fde7fb5c9adb1563c8cc9ca4a754a2b11d7
Neek Sandhu <neek.sandhu@outlook.com>
2022-09-22 10:34:42 -0700

    Reliable and safer messaging
    Fixes https://github.com/zikaari/webext-bridge/issues/23
    
    Each message sent now relies on delivery receipts to ensure they were
    indeed delivered AND to expect the destination's response. In case the
    destination disconnects before it could have responded, sendMessage
    Promise will be rejected with an 'Transaction ended before it could
    complete'.
    
    Queued messages are also no longer tracked in the background page, since
    it is ephemeral in manifest v3 extensions. Each endpoint now keeps track
    of its queued messages, and responses it is waiting on, in its on own
    runtime. This new strategy also acts as a safe-guard against
    destinations receiving stale messages from endpoints that no longer
    exist __(previsouly this could happen as there was no message invalidation
    logic for when the sender disconneted)__.
    
    The role of background page is no longer to maintain the "state" of the
    system, but to just act as a relay and to inform the relevant endpoints
    when the destination they are transacting with have been disconnected.

--- src/background.ts
+++ src/background.ts
@@ -1,87 +1,328 @@
 import browser from 'webextension-polyfill'
 import type { Runtime } from 'webextension-polyfill'
-import type { IInternalMessage } from './types'
 import { createEndpointRuntime } from './internal/endpoint-runtime'
 import { formatEndpoint, parseEndpoint } from './internal/endpoint'
 import { createStreamWirings } from './internal/stream'
+import type { EndpointFingerprint } from './internal/endpoint-fingerprint'
+import { createFingerprint } from './internal/endpoint-fingerprint'
+import { decodeConnectionArgs } from './internal/connection-args'
+import type { DeliveryReceipt } from './internal/delivery-logger'
+import { createDeliveryLogger } from './internal/delivery-logger'
+import type { RequestMessage } from './internal/port-message'
+import { PortMessage } from './internal/port-message'
+import type { InternalMessage } from './types'
 
-interface IQueuedMessage {
-  resolvedDestination: string
-  message: IInternalMessage
+interface PortConnection {
+  port: Runtime.Port
+  fingerprint: EndpointFingerprint
 }
 
-const messageQueue = new Set<IQueuedMessage>()
-const portMap = new Map<string, Runtime.Port>()
+const pendingResponses = createDeliveryLogger()
+const connMap = new Map<string, PortConnection>()
+const oncePortConnectedCbs = new Map<string, Set<() => void>>()
+const onceSessionEndCbs = new Map<EndpointFingerprint, Set<() => void>>()
+
+const oncePortConnected = (endpointName: string, cb: () => void) => {
+  oncePortConnectedCbs.set(
+    endpointName,
+    (oncePortConnectedCbs.get(endpointName) || new Set()).add(cb),
+  )
+
+  return () => {
+    const su = oncePortConnectedCbs.get(endpointName)
+    if (su?.delete(cb) && su?.size === 0)
+      oncePortConnectedCbs.delete(endpointName)
+  }
+}
+
+const onceSessionEnded = (
+  sessionFingerprint: EndpointFingerprint,
+  cb: () => void,
+) => {
+  onceSessionEndCbs.set(
+    sessionFingerprint,
+    (onceSessionEndCbs.get(sessionFingerprint) || new Set()).add(cb),
+  )
+}
+
+const notifyEndpoint = (endpoint: string) => ({
+  withFingerprint: (fingerprint: EndpointFingerprint) => {
+    const nextChain = <T>(v: T) => ({ and: () => v })
+
+    const notifications = {
+      aboutIncomingMessage: (message: InternalMessage) => {
+        const recipient = connMap.get(endpoint)
+
+        PortMessage.toExtensionContext(recipient.port, {
+          status: 'incoming',
+          message,
+        })
+
+        return nextChain(notifications)
+      },
+
+      aboutSuccessfulDelivery: (receipt: DeliveryReceipt) => {
+        const sender = connMap.get(endpoint)
+        PortMessage.toExtensionContext(sender.port, {
+          status: 'delivered',
+          receipt,
+        })
+
+        return nextChain(notifications)
+      },
+
+      aboutMessageUndeliverability: (
+        resolvedDestination: string,
+        message: InternalMessage,
+      ) => {
+        const sender = connMap.get(endpoint)
+        if (sender?.fingerprint === fingerprint) {
+          PortMessage.toExtensionContext(sender.port, {
+            status: 'undeliverable',
+            resolvedDestination,
+            message,
+          })
+        }
+
+        return nextChain(notifications)
+      },
+
+      whenDeliverableTo: (targetEndpoint: string) => {
+        const notifyDeliverability = () => {
+          const origin = connMap.get(endpoint)
+          if (
+            origin?.fingerprint === fingerprint
+            && connMap.has(targetEndpoint)
+          ) {
+            PortMessage.toExtensionContext(origin.port, {
+              status: 'deliverable',
+              deliverableTo: targetEndpoint,
+            })
+
+            return true
+          }
+        }
+
+        if (!notifyDeliverability()) {
+          const unsub = oncePortConnected(targetEndpoint, notifyDeliverability)
+          onceSessionEnded(fingerprint, unsub)
+        }
+
+        return nextChain(notifications)
+      },
+
+      aboutSessionEnded: (endedSessionFingerprint: EndpointFingerprint) => {
+        const conn = connMap.get(endpoint)
+        if (conn?.fingerprint === fingerprint) {
+          PortMessage.toExtensionContext(conn.port, {
+            status: 'terminated',
+            fingerprint: endedSessionFingerprint,
+          })
+        }
+
+        return nextChain(notifications)
+      },
+    }
+
+    return notifications
+  },
+})
+
+const sessFingerprint = createFingerprint()
 
 const endpointRuntime = createEndpointRuntime(
   'background',
   (message) => {
-    if (message.origin.context === 'background' && message.destination.context !== 'background' && !message.destination.tabId)
-      throw new TypeError('When sending messages from background page, use @tabId syntax to target specific tab')
+    if (
+      message.origin.context === 'background'
+      && message.destination.context !== 'background'
+      && !message.destination.tabId
+    ) {
+      throw new TypeError(
+        'When sending messages from background page, use @tabId syntax to target specific tab',
+      )
+    }
 
-    const { context: destName, tabId: destTabId, frameId: destFrameId } = message.destination
-    const { tabId: srcTabId } = message.origin
+    const resolvedSender = formatEndpoint({
+      ...message.origin,
+      ...(message.origin.context === 'window' && { context: 'content-script' }),
+    })
+
+    const resolvedDestination = formatEndpoint({
+      ...message.destination,
+      ...(message.destination.context === 'window' && {
+        context: 'content-script',
+      }),
+      tabId: message.destination.tabId || message.origin.tabId,
+    })
 
     // downstream endpoints are agnostic of these attributes, presence of these attrs will make them think the message is not intended for them
     message.destination.tabId = null
     message.destination.frameId = null
 
-    let resolvedDestination = ['popup', 'options'].includes(destName)
-      ? destName
-      : `${(destName === 'window' ? 'content-script' : destName)}@${(destTabId || srcTabId)}`
+    const dest = connMap.get(resolvedDestination)
+    const sender = connMap.get(resolvedSender)
 
-    // Here it is checked if a specific frame needs to receive the message
-    if (destFrameId)
-      resolvedDestination = `${resolvedDestination}.${destFrameId}`
+    const deliver = () => {
+      notifyEndpoint(resolvedDestination)
+        .withFingerprint(dest.fingerprint)
+        .aboutIncomingMessage(message)
 
-    const destPort = portMap.get(resolvedDestination)
+      const receipt: DeliveryReceipt = {
+        message,
+        to: dest.fingerprint,
+        from: {
+          endpointId: resolvedSender,
+          fingerprint: sender?.fingerprint,
+        },
+      }
 
-    if (destPort) destPort.postMessage(message)
-    else messageQueue.add({ resolvedDestination, message })
+      if (message.messageType === 'message') pendingResponses.add(receipt)
+
+      if (message.messageType === 'reply')
+        pendingResponses.remove(message.messageID)
+
+      if (sender) {
+        notifyEndpoint(resolvedSender)
+          .withFingerprint(sender.fingerprint)
+          .aboutSuccessfulDelivery(receipt)
+      }
+    }
+
+    if (dest?.port) {
+      deliver()
+    }
+    else if (message.messageType === 'message') {
+      if (message.origin.context === 'background') {
+        oncePortConnected(resolvedDestination, deliver)
+      }
+      else if (sender) {
+        notifyEndpoint(resolvedSender)
+          .withFingerprint(sender.fingerprint)
+          .aboutMessageUndeliverability(resolvedDestination, message)
+          .and()
+          .whenDeliverableTo(resolvedDestination)
+      }
+    }
+  },
+  (message) => {
+    const resolvedSender = formatEndpoint({
+      ...message.origin,
+      ...(message.origin.context === 'window' && { context: 'content-script' }),
+    })
+
+    const sender = connMap.get(resolvedSender)
+
+    const receipt: DeliveryReceipt = {
+      message,
+      to: sessFingerprint,
+      from: {
+        endpointId: resolvedSender,
+        fingerprint: sender.fingerprint,
+      },
+    }
+
+    notifyEndpoint(resolvedSender)
+      .withFingerprint(sender.fingerprint)
+      .aboutSuccessfulDelivery(receipt)
   },
 )
 
 browser.runtime.onConnect.addListener((incomingPort) => {
+  const connArgs = decodeConnectionArgs(incomingPort.name)
+
+  if (!connArgs) return
+
   // all other contexts except 'content-script' are aware of, and pass their identity as name
-  const portId = incomingPort.name || formatEndpoint({
+  connArgs.endpointName ||= formatEndpoint({
     context: 'content-script',
     tabId: incomingPort.sender.tab.id,
     frameId: incomingPort.sender.frameId,
   })
 
   // literal tab id in case of content script, however tab id of inspected page in case of devtools context
-  const { context, tabId: linkedTabId, frameId: linkedFrameId } = parseEndpoint(portId)
+  const { tabId: linkedTabId, frameId: linkedFrameId } = parseEndpoint(
+    connArgs.endpointName,
+  )
 
-  // in-case the port handshake is from something else
-  if (!linkedTabId && context !== 'popup' && context !== 'options')
-    return
+  connMap.set(connArgs.endpointName, {
+    fingerprint: connArgs.fingerprint,
+    port: incomingPort,
+  })
 
-  portMap.set(portId, incomingPort)
+  oncePortConnectedCbs.get(connArgs.endpointName)?.forEach(cb => cb())
+  oncePortConnectedCbs.delete(connArgs.endpointName)
 
-  messageQueue.forEach((queuedMsg) => {
-    if (queuedMsg.resolvedDestination === portId) {
-      incomingPort.postMessage(queuedMsg.message)
-      messageQueue.delete(queuedMsg)
-    }
+  onceSessionEnded(connArgs.fingerprint, () => {
+    const rogueMsgs = pendingResponses
+      .entries()
+      .filter(pendingMessage => pendingMessage.to === connArgs.fingerprint)
+    pendingResponses.remove(rogueMsgs)
+
+    rogueMsgs.forEach((rogueMessage) => {
+      if (rogueMessage.from.endpointId === 'background') {
+        endpointRuntime.endTransaction(rogueMessage.message.transactionId)
+      }
+      else {
+        notifyEndpoint(rogueMessage.from.endpointId)
+          .withFingerprint(rogueMessage.from.fingerprint)
+          .aboutSessionEnded(connArgs.fingerprint)
+      }
+    })
   })
 
   incomingPort.onDisconnect.addListener(() => {
     // sometimes previous content script's onDisconnect is called *after* the fresh content-script's
-    // onConnect. So without this reference equality check, we would remove the new port from map
-    if (portMap.get(portId) === incomingPort)
-      portMap.delete(portId)
+    // onConnect. So without this fingerprint equality check, we would remove the new port from map
+    if (
+      connMap.get(connArgs.endpointName)?.fingerprint === connArgs.fingerprint
+    )
+      connMap.delete(connArgs.endpointName)
+
+    onceSessionEndCbs.get(connArgs.fingerprint)?.forEach(cb => cb())
+    onceSessionEndCbs.delete(connArgs.fingerprint)
   })
 
-  incomingPort.onMessage.addListener((message: IInternalMessage) => {
-    if (message?.origin?.context) {
-      // origin tab ID is resolved from the port identifier (also prevent "MITM attacks" of extensions)
-      message.origin.tabId = linkedTabId
-      message.origin.frameId = linkedFrameId
+  incomingPort.onMessage.addListener((msg: RequestMessage) => {
+    if (msg.type === 'sync') {
+      const allActiveSessions = [...connMap.values()].map(
+        conn => conn.fingerprint,
+      )
+      const stillPending = msg.pendingResponses.filter(fp =>
+        allActiveSessions.includes(fp.to),
+      )
 
-      endpointRuntime.handleMessage(message)
+      pendingResponses.add(...stillPending)
+
+      msg.pendingResponses
+        .filter(
+          deliveryReceipt => !allActiveSessions.includes(deliveryReceipt.to),
+        )
+        .forEach(deliveryReceipt =>
+          notifyEndpoint(connArgs.endpointName)
+            .withFingerprint(connArgs.fingerprint)
+            .aboutSessionEnded(deliveryReceipt.to),
+        )
+
+      msg.pendingDeliveries.forEach(intendedDestination =>
+        notifyEndpoint(connArgs.endpointName)
+          .withFingerprint(connArgs.fingerprint)
+          .whenDeliverableTo(intendedDestination),
+      )
+
+      return
+    }
+
+    if (msg.type === 'deliver' && msg.message?.origin?.context) {
+      // origin tab ID is resolved from the port identifier (also prevent "MITM attacks" of extensions)
+      msg.message.origin.tabId = linkedTabId
+      msg.message.origin.frameId = linkedFrameId
+
+      endpointRuntime.handleMessage(msg.message)
     }
   })
 })
 
 export const { sendMessage, onMessage } = endpointRuntime
-export const { openStream, onOpenStreamChannel } = createStreamWirings(endpointRuntime)
+export const { openStream, onOpenStreamChannel }
+  = createStreamWirings(endpointRuntime)
--- src/internal/persistent-port.ts
+++ src/internal/persistent-port.ts
@@ -1,5 +1,12 @@
 import browser from 'webextension-polyfill'
 import type { Runtime } from 'webextension-polyfill'
+import type { InternalMessage } from '../types'
+import { createFingerprint } from './endpoint-fingerprint'
+import type { QueuedMessage } from './types'
+import { encodeConnectionArgs } from './connection-args'
+import { createDeliveryLogger } from './delivery-logger'
+import type { StatusMessage } from './port-message'
+import { PortMessage } from './port-message'
 
 /**
  * Manfiest V3 extensions can have their service worker terminated at any point
@@ -9,27 +16,111 @@ import type { Runtime } from 'webextension-polyfill'
  * suspended
  */
 export const createPersistentPort = (name = '') => {
+  const fingerprint = createFingerprint()
   let port: Runtime.Port
-  const onMessageListeners = new Set<(message: any, port: Runtime.Port) => void>()
+  let undeliveredQueue: ReadonlyArray<QueuedMessage> = []
+  const pendingResponses = createDeliveryLogger()
+  const onMessageListeners = new Set<
+  (message: InternalMessage, port: Runtime.Port) => void
+  >()
+  const onFailureListeners = new Set<(message: InternalMessage) => void>()
 
-  const invokeOnMessageListeners = (message: any, port: Runtime.Port) => {
-    onMessageListeners.forEach(cb => cb(message, port))
+  const handleMessage = (msg: StatusMessage, port: Runtime.Port) => {
+    switch (msg.status) {
+      case 'undeliverable':
+        if (
+          !undeliveredQueue.some(
+            m => m.message.messageID === msg.message.messageID,
+          )
+        ) {
+          undeliveredQueue = [
+            ...undeliveredQueue,
+            {
+              message: msg.message,
+              resolvedDestination: msg.resolvedDestination,
+            },
+          ]
+        }
+
+        return
+
+      case 'deliverable':
+        undeliveredQueue = undeliveredQueue.reduce((acc, queuedMsg) => {
+          if (queuedMsg.resolvedDestination === msg.deliverableTo) {
+            PortMessage.toBackground(port, {
+              type: 'deliver',
+              message: queuedMsg.message,
+            })
+
+            return acc
+          }
+
+          return [...acc, queuedMsg]
+        }, [] as ReadonlyArray<QueuedMessage>)
+
+        return
+
+      case 'delivered':
+        if (msg.receipt.message.messageType === 'message')
+          pendingResponses.add(msg.receipt)
+
+        return
+
+      case 'incoming':
+        if (msg.message.messageType === 'reply')
+          pendingResponses.remove(msg.message.messageID)
+
+        onMessageListeners.forEach(cb => cb(msg.message, port))
+
+        return
+
+      case 'terminated': {
+        const rogueMsgs = pendingResponses
+          .entries()
+          .filter(receipt => msg.fingerprint === receipt.to)
+        pendingResponses.remove(rogueMsgs)
+        rogueMsgs.forEach(({ message }) =>
+          onFailureListeners.forEach(cb => cb(message)),
+        )
+      }
+    }
   }
 
   const connect = () => {
-    port = browser.runtime.connect({ name })
-    port.onMessage.addListener(invokeOnMessageListeners)
+    port = browser.runtime.connect({
+      name: encodeConnectionArgs({
+        endpointName: name,
+        fingerprint,
+      }),
+    })
+    port.onMessage.addListener(handleMessage)
     port.onDisconnect.addListener(connect)
+
+    PortMessage.toBackground(port, {
+      type: 'sync',
+      pendingResponses: pendingResponses.entries(),
+      pendingDeliveries: [
+        ...new Set(
+          undeliveredQueue.map(({ resolvedDestination }) => resolvedDestination),
+        ),
+      ],
+    })
   }
 
   connect()
 
   return {
-    onMessage(cb: (message: any, port: Runtime.Port) => void): void {
+    onFailure(cb: (message: InternalMessage) => void) {
+      onFailureListeners.add(cb)
+    },
+    onMessage(cb: (message: InternalMessage) => void): void {
       onMessageListeners.add(cb)
     },
     postMessage(message: any): void {
-      port.postMessage(message)
+      PortMessage.toBackground(port, {
+        type: 'deliver',
+        message,
+      })
     },
   }
 }
