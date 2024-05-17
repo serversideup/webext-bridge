27c7d456a85960338586853d50607dc39addc537
Dan Pastori <dan@521dimensions.com>
2024-05-17 09:46:30 -0700

    Migrated over components and set up initial pages

---
+++ docs/components/Content/MarketingServers.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mx-auto max-w-[940px] flex flex-col mt-8 md:mt-16">
+        <h2 class="w-full text-center font-inter text-white text-3xl md:text-5xl font-bold max-w-xl mx-auto">💪 Manage servers right from your project</h2>
+        <p class="w-full text-center mt-6 text-slate-300 font-inter text-xl md:text-2xl">Control and own your infrastructure without the headache.</p>
+
+        <div class="px-4">
+            <div class="rounded-[10px] mx-auto max-w-[940px] my-8 md:my-16 overflow-hidden">
+                <video autoplay muted loop playsinline>
+                    <source src="https://spin-public-assets.serversideup.net/spin-demo_spin-provision.mp4"/>
+                </video>
+            </div>
+        </div>
+    </div>
+</template>
